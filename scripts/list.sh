47fd25e605abf716b407c793b9644adb6fab6456
norakjed <norakjadchhorn@gmail.com>
2025-03-25 21:17:27 +0700

    updated script for list.sh

--- scripts/list.sh
+++ scripts/list.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-	
-echo "Enter file:"list_file() {
-	read -p "Enter the directory or file path: " path
-	if [ -e "$path" ]; then
-		ls -lah "$path"
-	else 
-		echo "Error!! Path does not exist!"
-	fi
-}
+
+read -p "Enter the directory or file path: " path
+if [ -e "$path" ]; then
+	ls -lah "$path"
+else 
+	echo "Error!! Path does not exist!"
+fi
+
